0774e1270823699b648960a4405dfbc08358ed0c
soo24 <suyeon8946@gmail.com>
2023-01-10 20:19:24 +0900

    Add /id를 지정해 이름지정 _2-6강

--- travel-club-spring-core/src/main/java/io/namoosori/travelclub/spring/service/logic/ClubServiceLogic.java
+++ travel-club-spring-core/src/main/java/io/namoosori/travelclub/spring/service/logic/ClubServiceLogic.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+@Service("clubservice")
 public class ClubServiceLogic implements ClubService {
 
     //field 선언
